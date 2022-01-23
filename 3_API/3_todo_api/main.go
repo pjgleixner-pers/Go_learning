6f9af62e8fb98fa838f6ef9596c827aadc31041c
Pieter Gleixner <pjgleixner@msn.com>
2022-01-22 20:31:38 -0500

    api connection to database

--- 3_API/3_todo_api/main.go
+++ 3_API/3_todo_api/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"api/3_todo_api/controller"
+	"api/3_todo_api/model"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := controller.Register()
-	http.ListenAndServe(":3000", mux)
+	db := model.Connect()
+	defer db.Close()
+	fmt.Println("Serving...")
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
---
+++ 3_API/3_todo_api/model/connect.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var con *sql.DB
+
+func Connect() *sql.DB {
+	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/mysql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to the database")
+	con = db
+	return db
+}
